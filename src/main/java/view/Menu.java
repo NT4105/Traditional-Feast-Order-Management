55ffb4d70fd86b63cbefad90d5edb19019999a6c
NT4105 <tunguyen04105@gmail.com>
2025-03-09 00:00:10 +0700

    Add Menu view class and FeastMenu CSV resource

---
+++ src/main/java/view/Menu.java
@@ -0,0 +1,25 @@
+package view;
+
+public class Menu {
+    public void MainMenu() {
+        System.out.println("*********************************************************");
+        System.out.println("*                                                       *");
+        System.out.println("*        Traditional Feast Order Management             *");
+        System.out.println("*                    Main Menu                          *");
+        System.out.println("*                                                       *");
+        System.out.println("*********************************************************");
+        System.out.println("* 1. Register customer                                  *");
+        System.out.println("* 2. Update customer information                        *");
+        System.out.println("* 3. Search for customer information by name            *");
+        System.out.println("* 4. Display feast menus                                *");
+        System.out.println("* 5. Place a feast order                                *");
+        System.out.println("* 6. Update order information                           *");
+        System.out.println("* 7. Save Data to File                                  *");
+        System.out.println("* 8. Display Customers or Order lists                   *");
+        System.out.println("* 9. Exit Program                                       *");
+        System.out.println("*********************************************************");
+        System.out.println("* Please enter your choice (1-9):                       *");
+        System.out.println("*********************************************************");
+    }
+
+}
